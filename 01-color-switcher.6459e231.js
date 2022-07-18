6777a54abb4ff541925a6aa8bc89da40784f4182
YDovzhyk <48929257+YDovzhyk@users.noreply.github.com>
2022-07-18 13:27:24 +0000

    Deploying to gh-pages from @ YDovzhyk/goit-js-hw-09@ed8ef76ee74ba9b94c60c27efc2548c3ffb334b8 🚀

--- 01-color-switcher.6459e231.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("body"),t=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let o=null;t.addEventListener("click",(()=>{o=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),r.addEventListener("click",(()=>{clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.6459e231.js.map
--- 01-color-switcher.8d9ca237.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),o=null;e.addEventListener("click",(function(){o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.8d9ca237.js.map
